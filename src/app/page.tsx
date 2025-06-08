10ae64444e2401c2643717f0b233292395579c87
Free Guy <guyf9419@gmail.com>
2025-06-08 06:30:05 +0000

    let's disable authentication first, I need to test layout and feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/login');
+  redirect('/admin/dashboard'); // Redirect to Admin Dashboard for testing
   return null; 
 }
